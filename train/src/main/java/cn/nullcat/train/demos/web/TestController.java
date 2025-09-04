4c14d05ff805926367213cee0a4c5fba24b064bf
Nu11Cat <2111867383@qq.com>
2025-09-04 20:17:20 +0800

    初始化

---
+++ train/src/main/java/cn/nullcat/train/demos/web/TestController.java
@@ -0,0 +1,12 @@
+package cn.nullcat.train.demos.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
