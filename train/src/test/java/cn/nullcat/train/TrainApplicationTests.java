db924d90a781dd3c0593bb2ea9be013e00c23aea
Nu11Cat <2111867383@qq.com>
2025-09-04 20:17:20 +0800

    初始化

---
+++ train/src/test/java/cn/nullcat/train/TrainApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.nullcat.train;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
