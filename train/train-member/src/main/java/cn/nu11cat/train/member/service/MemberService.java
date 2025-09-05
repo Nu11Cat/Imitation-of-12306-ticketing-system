b3de83b9d0dd322d2746b60c43e5a9655adb32e3
Nu11Cat <2111867383@qq.com>
2025-09-05 16:44:17 +0800

    feat：添加自定义异常处理义务异常，处理“会员注册”自定义业务异常

--- train/train-member/src/main/java/cn/nu11cat/train/member/service/MemberService.java
+++ train/train-member/src/main/java/cn/nu11cat/train/member/service/MemberService.java
@@ -1,6 +1,8 @@
 package cn.nu11cat.train.member.service;
 
 import cn.hutool.core.collection.CollUtil;
+import cn.nu11cat.train.common.exception.BusinessException;
+import cn.nu11cat.train.common.exception.BusinessExceptionEnum;
 import cn.nu11cat.train.member.domain.Member;
 import cn.nu11cat.train.member.domain.MemberExample;
 import cn.nu11cat.train.member.mapper.MemberMapper;
@@ -33,7 +35,8 @@ public class MemberService {
         List<Member> members = memberMapper.selectByExample(memberExample);
         if(CollUtil.isNotEmpty(members)) {
             //return members.get(0).getId();
-            throw new RuntimeException("手机号已注册");
+            //throw new RuntimeException("手机号已注册");
+            throw new BusinessException(BusinessExceptionEnum.MEMBER_MOBILE_EXIST);
         }
         //新增会员
         Member member1 = new Member();
