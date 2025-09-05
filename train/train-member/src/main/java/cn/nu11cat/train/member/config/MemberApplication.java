c75f144c99fea9e8db62050afca7e960cffe536c
Nu11Cat <2111867383@qq.com>
2025-09-05 13:49:44 +0800

    feat：集成mybatis、添加mapper数据库日志，访问数据库成功。

--- train/train-member/src/main/java/cn/nu11cat/train/member/config/MemberApplication.java
+++ train/train-member/src/main/java/cn/nu11cat/train/member/config/MemberApplication.java
@@ -1,5 +1,6 @@
 package cn.nu11cat.train.member.config;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -9,6 +10,7 @@ import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("cn.nu11cat")
+@MapperScan("cn.nu11cat.train.member.mapper")
 public class MemberApplication {
     private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
 
---
+++ train/train-member/src/main/java/cn/nu11cat/train/member/controller/MemberController.java
@@ -0,0 +1,20 @@
+package cn.nu11cat.train.member.controller;
+
+import cn.nu11cat.train.member.service.MemberService;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/member")
+public class MemberController {
+
+    @Resource
+    private MemberService memberService;
+
+    @GetMapping("/count")
+    public int count(){
+        return memberService.count();
+    }
+}
