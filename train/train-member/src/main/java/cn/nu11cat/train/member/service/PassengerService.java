25423b1c281ccb6a0d8b63d38ce73e580302ea12
Nu11Cat <2111867383@qq.com>
2025-09-06 17:25:17 +0800

    feat：增加日志拦截器，使用线程本地变量存储会员信息

--- train/train-member/src/main/java/cn/nu11cat/train/member/service/PassengerService.java
+++ train/train-member/src/main/java/cn/nu11cat/train/member/service/PassengerService.java
@@ -2,6 +2,7 @@ package cn.nu11cat.train.member.service;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
+import cn.nu11cat.train.common.context.LoginMemberContext;
 import cn.nu11cat.train.common.util.SnowUtil;
 import cn.nu11cat.train.member.domain.Passenger;
 import cn.nu11cat.train.member.mapper.PassengerMapper;
@@ -22,6 +23,7 @@ public class PassengerService {
     public void savePassenger(PassengerSaveReq req){
         DateTime now = DateTime.now();
         Passenger passenger = BeanUtil.copyProperties(req, Passenger.class);
+        passenger.setMemberId(LoginMemberContext.getId());
         passenger.setId(SnowUtil.getSnowflakeNextId());
         passenger.setCreateTime(now);
         passenger.setUpdateTime(now);
