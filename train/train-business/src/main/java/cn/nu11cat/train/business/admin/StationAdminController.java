c3fa79bbbf7f043292dd866d531ad4b64a04834e
Nu11Cat <2111867383@qq.com>
2025-09-08 22:08:00 +0800

    feat：完成admin [车站] 相关增删改查接口

---
+++ train/train-business/src/main/java/cn/nu11cat/train/business/admin/StationAdminController.java
@@ -0,0 +1,38 @@
+package cn.nu11cat.train.business.admin;
+
+import cn.nu11cat.train.business.req.StationQueryReq;
+import cn.nu11cat.train.business.req.StationSaveReq;
+import cn.nu11cat.train.business.resp.StationQueryResp;
+import cn.nu11cat.train.business.service.StationService;
+import cn.nu11cat.train.common.resp.CommonResp;
+import cn.nu11cat.train.common.resp.PageResp;
+import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/admin/station")
+public class StationAdminController {
+
+    @Resource
+    private StationService stationService;
+
+    @PostMapping("/save")
+    public CommonResp<Object> save(@Valid @RequestBody StationSaveReq req) {
+        stationService.save(req);
+        return new CommonResp<>();
+    }
+
+    @GetMapping("/query-list")
+    public CommonResp<PageResp<StationQueryResp>> queryList(@Valid StationQueryReq req) {
+        PageResp<StationQueryResp> list = stationService.queryList(req);
+        return new CommonResp<>(list);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp<Object> delete(@PathVariable Long id) {
+        stationService.delete(id);
+        return new CommonResp<>();
+    }
+
+}
