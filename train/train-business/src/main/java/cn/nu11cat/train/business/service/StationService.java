3d4d3dcb336c37758476eeba15fd274e341af0de
Nu11Cat <2111867383@qq.com>
2025-09-09 15:27:26 +0800

    feat：添加 [车站新增] 的存在性校验

--- train/train-business/src/main/java/cn/nu11cat/train/business/service/StationService.java
+++ train/train-business/src/main/java/cn/nu11cat/train/business/service/StationService.java
@@ -1,6 +1,7 @@
 package cn.nu11cat.train.business.service;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.util.ObjectUtil;
 import cn.nu11cat.train.business.domain.Station;
@@ -9,6 +10,8 @@ import cn.nu11cat.train.business.mapper.StationMapper;
 import cn.nu11cat.train.business.req.StationQueryReq;
 import cn.nu11cat.train.business.req.StationSaveReq;
 import cn.nu11cat.train.business.resp.StationQueryResp;
+import cn.nu11cat.train.common.exception.BusinessException;
+import cn.nu11cat.train.common.exception.BusinessExceptionEnum;
 import cn.nu11cat.train.common.resp.PageResp;
 import cn.nu11cat.train.common.util.SnowUtil;
 import com.github.pagehelper.PageHelper;
@@ -32,6 +35,13 @@ public class StationService {
         DateTime now = DateTime.now();
         Station station = BeanUtil.copyProperties(req, Station.class);
         if (ObjectUtil.isNull(station.getId())) {
+
+            // 保存之前，先校验唯一键是否存在
+            Station stationDB = selectByUnique(req.getName());
+            if (ObjectUtil.isNotEmpty(stationDB)) {
+                throw new BusinessException(BusinessExceptionEnum.BUSINESS_STATION_NAME_UNIQUE_ERROR);
+            }
+
             station.setId(SnowUtil.getSnowflakeNextId());
             station.setCreateTime(now);
             station.setUpdateTime(now);
@@ -39,6 +49,17 @@ public class StationService {
         } else {
             station.setUpdateTime(now);
             stationMapper.updateByPrimaryKey(station);
+        }
+    }
+
+    private Station selectByUnique(String name) {
+        StationExample stationExample = new StationExample();
+        stationExample.createCriteria().andNameEqualTo(name);
+        List<Station> list = stationMapper.selectByExample(stationExample);
+        if (CollUtil.isNotEmpty(list)) {
+            return list.get(0);
+        } else {
+            return null;
         }
     }
 
