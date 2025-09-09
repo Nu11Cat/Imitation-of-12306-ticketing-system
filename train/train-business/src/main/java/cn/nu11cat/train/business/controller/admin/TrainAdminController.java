4d5acdd9ee6ed2e26adf3685cd7d2ce438e47d85
Nu11Cat <2111867383@qq.com>
2025-09-09 10:37:01 +0800

    feat：添加查询全部车次编号功能

--- train/train-business/src/main/java/cn/nu11cat/train/business/controller/admin/TrainAdminController.java
+++ train/train-business/src/main/java/cn/nu11cat/train/business/controller/admin/TrainAdminController.java
@@ -1,15 +1,16 @@
 package cn.nu11cat.train.business.controller.admin;
 
-import cn.nu11cat.train.common.context.LoginMemberContext;
-import cn.nu11cat.train.common.resp.CommonResp;
-import cn.nu11cat.train.common.resp.PageResp;
 import cn.nu11cat.train.business.req.TrainQueryReq;
 import cn.nu11cat.train.business.req.TrainSaveReq;
 import cn.nu11cat.train.business.resp.TrainQueryResp;
 import cn.nu11cat.train.business.service.TrainService;
+import cn.nu11cat.train.common.resp.CommonResp;
+import cn.nu11cat.train.common.resp.PageResp;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin/train")
@@ -34,6 +35,12 @@ public class TrainAdminController {
     public CommonResp<Object> delete(@PathVariable Long id) {
         trainService.delete(id);
         return new CommonResp<>();
+    }
+
+    @GetMapping("/query-all")
+    public CommonResp<List<TrainQueryResp>> queryList() {
+        List<TrainQueryResp> list = trainService.queryAll();
+        return new CommonResp<>(list);
     }
 
 }
