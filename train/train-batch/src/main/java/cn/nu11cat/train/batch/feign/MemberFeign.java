e943788f9eb742d4ca0371ae9249031685d6554a
Nu11Cat <2111867383@qq.com>
2025-09-09 19:58:22 +0800

    feat：添加数据库表配置控制quartz调度任务

---
+++ train/train-batch/src/main/java/cn/nu11cat/train/batch/feign/MemberFeign.java
@@ -0,0 +1,13 @@
+package cn.nu11cat.train.batch.feign;
+
+import cn.nu11cat.train.common.resp.CommonResp;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(value = "member")
+public interface MemberFeign {
+
+    @GetMapping("/member/passenger/init")
+    CommonResp init();
+
+}
