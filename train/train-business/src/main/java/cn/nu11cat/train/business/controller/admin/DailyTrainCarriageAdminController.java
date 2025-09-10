7201aa8a6b1702a0a5d6fa71a1d35e49def20a2e
Nu11Cat <2111867383@qq.com>
2025-09-10 13:30:30 +0800

    feat：新增 [每日车厢]的功能，增加[日期][车次]的查询功能，自动计算填充座位数和列数

---
+++ train/train-business/src/main/java/cn/nu11cat/train/business/controller/admin/DailyTrainCarriageAdminController.java
@@ -0,0 +1,39 @@
+package cn.nu11cat.train.business.controller.admin;
+
+import cn.nu11cat.train.common.context.LoginMemberContext;
+import cn.nu11cat.train.common.resp.CommonResp;
+import cn.nu11cat.train.common.resp.PageResp;
+import cn.nu11cat.train.business.req.DailyTrainCarriageQueryReq;
+import cn.nu11cat.train.business.req.DailyTrainCarriageSaveReq;
+import cn.nu11cat.train.business.resp.DailyTrainCarriageQueryResp;
+import cn.nu11cat.train.business.service.DailyTrainCarriageService;
+import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/admin/daily-train-carriage")
+public class DailyTrainCarriageAdminController {
+
+    @Resource
+    private DailyTrainCarriageService dailyTrainCarriageService;
+
+    @PostMapping("/save")
+    public CommonResp<Object> save(@Valid @RequestBody DailyTrainCarriageSaveReq req) {
+        dailyTrainCarriageService.save(req);
+        return new CommonResp<>();
+    }
+
+    @GetMapping("/query-list")
+    public CommonResp<PageResp<DailyTrainCarriageQueryResp>> queryList(@Valid DailyTrainCarriageQueryReq req) {
+        PageResp<DailyTrainCarriageQueryResp> list = dailyTrainCarriageService.queryList(req);
+        return new CommonResp<>(list);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp<Object> delete(@PathVariable Long id) {
+        dailyTrainCarriageService.delete(id);
+        return new CommonResp<>();
+    }
+
+}
