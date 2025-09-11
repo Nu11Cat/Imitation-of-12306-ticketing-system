646dfd9e5285d6080a6c879dfce0a0fdbefa3ba3
Nu11Cat <2111867383@qq.com>
2025-09-11 16:22:04 +0800

    feat：新增[余票信息]相关管理的功能

---
+++ train/train-business/src/main/java/cn/nu11cat/train/business/controller/admin/DailyTrainTicketAdminController.java
@@ -0,0 +1,39 @@
+package cn.nu11cat.train.business.controller.admin;
+
+import cn.nu11cat.train.common.context.LoginMemberContext;
+import cn.nu11cat.train.common.resp.CommonResp;
+import cn.nu11cat.train.common.resp.PageResp;
+import cn.nu11cat.train.business.req.DailyTrainTicketQueryReq;
+import cn.nu11cat.train.business.req.DailyTrainTicketSaveReq;
+import cn.nu11cat.train.business.resp.DailyTrainTicketQueryResp;
+import cn.nu11cat.train.business.service.DailyTrainTicketService;
+import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/admin/daily-train-ticket")
+public class DailyTrainTicketAdminController {
+
+    @Resource
+    private DailyTrainTicketService dailyTrainTicketService;
+
+    @PostMapping("/save")
+    public CommonResp<Object> save(@Valid @RequestBody DailyTrainTicketSaveReq req) {
+        dailyTrainTicketService.save(req);
+        return new CommonResp<>();
+    }
+
+    @GetMapping("/query-list")
+    public CommonResp<PageResp<DailyTrainTicketQueryResp>> queryList(@Valid DailyTrainTicketQueryReq req) {
+        PageResp<DailyTrainTicketQueryResp> list = dailyTrainTicketService.queryList(req);
+        return new CommonResp<>(list);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp<Object> delete(@PathVariable Long id) {
+        dailyTrainTicketService.delete(id);
+        return new CommonResp<>();
+    }
+
+}
---
+++ train/train-business/src/main/java/cn/nu11cat/train/business/req/DailyTrainTicketQueryReq.java
@@ -0,0 +1,12 @@
+package cn.nu11cat.train.business.req;
+
+import cn.nu11cat.train.common.req.PageReq;
+
+public class DailyTrainTicketQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "DailyTrainTicketQueryReq{" +
+                "} " + super.toString();
+    }
+}
