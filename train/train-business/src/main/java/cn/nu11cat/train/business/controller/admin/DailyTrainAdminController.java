3f2f655b344e386ea86a6f65971fb98a633d2f09
Nu11Cat <2111867383@qq.com>
2025-09-11 13:54:46 +0800

    feat：新增生成 [每日车次]的功能

--- train/train-business/src/main/java/cn/nu11cat/train/business/controller/admin/DailyTrainAdminController.java
+++ train/train-business/src/main/java/cn/nu11cat/train/business/controller/admin/DailyTrainAdminController.java
@@ -1,15 +1,17 @@
 package cn.nu11cat.train.business.controller.admin;
 
-import cn.nu11cat.train.common.context.LoginMemberContext;
-import cn.nu11cat.train.common.resp.CommonResp;
-import cn.nu11cat.train.common.resp.PageResp;
 import cn.nu11cat.train.business.req.DailyTrainQueryReq;
 import cn.nu11cat.train.business.req.DailyTrainSaveReq;
 import cn.nu11cat.train.business.resp.DailyTrainQueryResp;
 import cn.nu11cat.train.business.service.DailyTrainService;
+import cn.nu11cat.train.common.resp.CommonResp;
+import cn.nu11cat.train.common.resp.PageResp;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
 
 @RestController
 @RequestMapping("/admin/daily-train")
@@ -33,6 +35,12 @@ public class DailyTrainAdminController {
     @DeleteMapping("/delete/{id}")
     public CommonResp<Object> delete(@PathVariable Long id) {
         dailyTrainService.delete(id);
+        return new CommonResp<>();
+    }
+
+    @GetMapping("/gen-daily/{date}")
+    public CommonResp<Object> genDaily(@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") Date date) {
+        dailyTrainService.genDaily(date);
         return new CommonResp<>();
     }
 
---
+++ train/train-business/src/main/java/cn/nu11cat/train/business/feign/MemberFeign.java
@@ -0,0 +1,16 @@
+package cn.nu11cat.train.business.feign;
+
+import cn.nu11cat.train.common.req.MemberTicketReq;
+import cn.nu11cat.train.common.resp.CommonResp;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+// @FeignClient("member")
+@FeignClient(name = "member", url = "http://127.0.0.1:8001")
+public interface MemberFeign {
+
+    @GetMapping("/member/feign/ticket/save")
+    CommonResp<Object> save(@RequestBody MemberTicketReq req);
+
+}
