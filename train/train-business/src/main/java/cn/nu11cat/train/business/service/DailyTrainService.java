a0609adf1ba5867205844278a4feee1427c13189
Nu11Cat <2111867383@qq.com>
2025-09-11 14:23:36 +0800

    feat：新增生成 [每日车站]的功能，新增生成 [每日车厢]的功能，新增生成 [每日座位]的功能

--- train/train-business/src/main/java/cn/nu11cat/train/business/service/DailyTrainService.java
+++ train/train-business/src/main/java/cn/nu11cat/train/business/service/DailyTrainService.java
@@ -134,13 +134,13 @@ public class DailyTrainService {
         dailyTrainMapper.insert(dailyTrain);
 
         // 生成该车次的车站数据
-        //dailyTrainStationService.genDaily(date, train.getCode());
+        dailyTrainStationService.genDaily(date, train.getCode());
 
         // 生成该车次的车厢数据
-        //dailyTrainCarriageService.genDaily(date, train.getCode());
+        dailyTrainCarriageService.genDaily(date, train.getCode());
 
         // 生成该车次的座位数据
-        //dailyTrainSeatService.genDaily(date, train.getCode());
+        dailyTrainSeatService.genDaily(date, train.getCode());
 
         // 生成该车次的余票数据
         //dailyTrainTicketService.genDaily(dailyTrain, date, train.getCode());
@@ -148,7 +148,7 @@ public class DailyTrainService {
         // 生成令牌余量数据
         //skTokenService.genDaily(date, train.getCode());
 
-        //LOG.info("生成日期【{}】车次【{}】的信息结束", DateUtil.formatDate(date), train.getCode());
+        LOG.info("生成日期【{}】车次【{}】的信息结束", DateUtil.formatDate(date), train.getCode());
     }
 
 }
