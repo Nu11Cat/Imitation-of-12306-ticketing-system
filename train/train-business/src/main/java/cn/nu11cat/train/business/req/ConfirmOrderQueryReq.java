9ac7ffdf29d86e933dfae3f5ed318f298138bb17
Nu11Cat <2111867383@qq.com>
2025-09-12 17:01:01 +0800

    feat：添加用户端 [确认订单] 功能，增加购票订单req

---
+++ train/train-business/src/main/java/cn/nu11cat/train/business/req/ConfirmOrderQueryReq.java
@@ -0,0 +1,12 @@
+package cn.nu11cat.train.business.req;
+
+import cn.nu11cat.train.common.req.PageReq;
+
+public class ConfirmOrderQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "ConfirmOrderQueryReq{" +
+                "} " + super.toString();
+    }
+}
