ac493494401467acc02049ac03021c6a16801c2e
Nu11Cat <2111867383@qq.com>
2025-09-13 22:03:24 +0800

    feat：添加 [乘客车票表] 相关代码，增加feign接口保存乘客车票信息

---
+++ train/train-member/src/main/java/cn/nu11cat/train/member/req/TicketQueryReq.java
@@ -0,0 +1,12 @@
+package cn.nu11cat.train.member.req;
+
+import cn.nu11cat.train.common.req.PageReq;
+
+public class TicketQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "TicketQueryReq{" +
+                "} " + super.toString();
+    }
+}
