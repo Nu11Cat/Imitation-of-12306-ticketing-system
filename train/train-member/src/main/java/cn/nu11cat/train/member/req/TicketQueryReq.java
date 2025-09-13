e9e2fcaa2869ea479f731589bf801dbac75b3414
Nu11Cat <2111867383@qq.com>
2025-09-13 22:15:18 +0800

    feat：增加会员端 [我的车票] 相关功能

--- train/train-member/src/main/java/cn/nu11cat/train/member/req/TicketQueryReq.java
+++ train/train-member/src/main/java/cn/nu11cat/train/member/req/TicketQueryReq.java
@@ -4,9 +4,20 @@ import cn.nu11cat.train.common.req.PageReq;
 
 public class TicketQueryReq extends PageReq {
 
+    private Long memberId;
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
     @Override
     public String toString() {
         return "TicketQueryReq{" +
+                "memberId=" + memberId +
                 "} " + super.toString();
     }
 }
