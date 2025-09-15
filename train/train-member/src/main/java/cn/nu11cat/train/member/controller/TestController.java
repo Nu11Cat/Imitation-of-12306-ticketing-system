6975eb3a96668a6eada83d77f5a038856d418fa0
Nu11Cat <2111867383@qq.com>
2025-09-15 16:22:41 +0800

    feat：读取nacos配置文件配置

--- train/train-member/src/main/java/cn/nu11cat/train/member/controller/TestController.java
+++ train/train-member/src/main/java/cn/nu11cat/train/member/controller/TestController.java
@@ -1,12 +1,22 @@
 package cn.nu11cat.train.member.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @Value("${test.nacos}")
+    private String testNacos;
+
+   // @Autowired
+    //Environment environment;
+
     @GetMapping("/hello")
-    public String hello(){
-        return "hello member";
+    public String hello() {
+        //String port = environment.getProperty("local.server.port");
+        //return String.format("Hello %s! 端口：%s", testNacos, port);
+        return String.format("Hello %s!", testNacos);
     }
 }
