e1088e69b94dc4afb430a6ce99ccd382e9c4200b
Nu11Cat <2111867383@qq.com>
2025-09-18 15:49:37 +0800

    feat: 添加令牌表，初始化令牌信息基础功能；完成初始化车次信息时初始化秒杀令牌信息功能；增加校验秒杀令牌功能;利用令牌锁防止机器人抢票;使用缓存加速令牌锁功能

---
+++ train/train-business/src/main/java/cn/nu11cat/train/business/controller/admin/SkTokenAdminController.java
@@ -0,0 +1,39 @@
+package cn.nu11cat.train.business.controller.admin;
+
+import cn.nu11cat.train.common.context.LoginMemberContext;
+import cn.nu11cat.train.common.resp.CommonResp;
+import cn.nu11cat.train.common.resp.PageResp;
+import cn.nu11cat.train.business.req.SkTokenQueryReq;
+import cn.nu11cat.train.business.req.SkTokenSaveReq;
+import cn.nu11cat.train.business.resp.SkTokenQueryResp;
+import cn.nu11cat.train.business.service.SkTokenService;
+import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/admin/sk-token")
+public class SkTokenAdminController {
+
+    @Resource
+    private SkTokenService skTokenService;
+
+    @PostMapping("/save")
+    public CommonResp<Object> save(@Valid @RequestBody SkTokenSaveReq req) {
+        skTokenService.save(req);
+        return new CommonResp<>();
+    }
+
+    @GetMapping("/query-list")
+    public CommonResp<PageResp<SkTokenQueryResp>> queryList(@Valid SkTokenQueryReq req) {
+        PageResp<SkTokenQueryResp> list = skTokenService.queryList(req);
+        return new CommonResp<>(list);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp<Object> delete(@PathVariable Long id) {
+        skTokenService.delete(id);
+        return new CommonResp<>();
+    }
+
+}
---
+++ train/train-business/src/main/java/cn/nu11cat/train/business/req/SkTokenQueryReq.java
@@ -0,0 +1,12 @@
+package cn.nu11cat.train.business.req;
+
+import cn.nu11cat.train.common.req.PageReq;
+
+public class SkTokenQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "SkTokenQueryReq{" +
+                "} " + super.toString();
+    }
+}
