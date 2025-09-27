3db4aa1a6a344acd6c77b943d52d4954fcdb1a80
Nu11Cat <2111867383@qq.com>
2025-09-27 15:53:29 +0800

    feat:train-mp代码重构，更改错误的文件名

--- mpdemo/src/main/java/cn/nu11cat/mpdemo/mapper/UserMapper.java
+++
@@ -1,8 +0,0 @@
-package cn.nu11cat.mpdemo.mapper;
-
-import cn.nu11cat.mpdemo.entity.User;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-
-public interface UserMapper extends BaseMapper<User> {
-
-}
--- mpdemo/src/main/java/cn/nu11cat/mpdemo/req/UserDeleteReq.java
+++
@@ -1,8 +0,0 @@
-package cn.nu11cat.mpdemo.req;
-
-import lombok.Data;
-
-@Data
-public class UserDeleteReq {
-    private String email; // 条件字段
-}
