3b6f8596da047f003472d1a0e67aa11c0462bbd5
Nu11Cat <2111867383@qq.com>
2025-09-27 16:38:27 +0800

    feat:train-mp代码重构，移植business模块代码，包括config、dto、enums、req、resp等。注释启动类sentinel，配置rocketmq

---
+++ train-mp/train-business/src/main/java/cn/nu11cat/train/business/req/TrainCarriageQueryReq.java
@@ -0,0 +1,23 @@
+package cn.nu11cat.train.business.req;
+
+import cn.nu11cat.train.common.req.PageReq;
+
+public class TrainCarriageQueryReq extends PageReq {
+
+    private String trainCode;
+
+    public String getTrainCode() {
+        return trainCode;
+    }
+
+    public void setTrainCode(String trainCode) {
+        this.trainCode = trainCode;
+    }
+
+    @Override
+    public String toString() {
+        return "TrainCarriageQueryReq{" +
+                "trainCode='" + trainCode + '\'' +
+                "} " + super.toString();
+    }
+}
