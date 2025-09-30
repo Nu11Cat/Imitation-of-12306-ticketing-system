58401492d6c7c06ed2643d7aa58864fc14558d91
Nu11Cat <2111867383@qq.com>
2025-09-30 17:22:24 +0800

    feat:v2重构后，压测版本

--- train-mp/train-business/src/main/java/cn/nu11cat/train/business/config/BusinessApplication.java
+++ train-mp/train-business/src/main/java/cn/nu11cat/train/business/config/BusinessApplication.java
@@ -33,7 +33,7 @@ public class BusinessApplication {
         LOG.info("启动成功！！");
         LOG.info("测试地址: \thttp://127.0.0.1:{}{}/hello", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
          // 限流规则
-         initFlowRules();
+         //initFlowRules();
          LOG.info("已定义限流规则");
     }
 
